591de5408abc053930bff03ecaead78af95fea0c
Brandon Lim <brandon.lim@zuhlke.com>
2021-11-09 13:17:32 +0800

    Change to use a constant.

--- src/main/java/com/layhill/roadsim/gameengine/Time.java
+++ src/main/java/com/layhill/roadsim/gameengine/Time.java
@@ -2,6 +2,7 @@ package com.layhill.roadsim.gameengine;
 
 public class Time {
 
+    public static final double NANO_TO_SECOND_MULTIPLIER = 1.0E-9;
     private double beginTime = 0;
     private double currentTime = 0;
     private double deltaTime;
@@ -11,7 +12,7 @@ public class Time {
     }
 
     private Time() {
-        beginTime = System.nanoTime() * 1.0E-9;
+        beginTime = System.nanoTime() * NANO_TO_SECOND_MULTIPLIER;
     }
 
     public static Time getInstance() {
@@ -28,7 +29,7 @@ public class Time {
 
     public void tick() {
         beginTime = currentTime;
-        currentTime = System.nanoTime() * 1.0E-9;
+        currentTime = System.nanoTime() * NANO_TO_SECOND_MULTIPLIER;
         if (beginTime > 0.0) {
             deltaTime = currentTime - beginTime;
         }
