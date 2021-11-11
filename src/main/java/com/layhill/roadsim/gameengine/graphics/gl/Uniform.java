fddb853db9b26fe9317b4e3e23f7249662d0b6a4
Brandon Lim <brandon.lim@zuhlke.com>
2021-11-11 18:58:10 +0800

    Start implementing uniform store

---
+++ src/main/java/com/layhill/roadsim/gameengine/graphics/gl/Uniform.java
@@ -0,0 +1,18 @@
+package com.layhill.roadsim.gameengine.graphics.gl;
+
+public abstract class Uniform {
+
+    private String name;
+
+    protected Uniform(String name) {
+        this.name = name;
+    }
+
+
+    public void uploadToUniformLocation(int programId) {
+
+    }
+
+    public void dispose() {
+    }
+}
