83c52d7e30a06962ee9e4d25372fdbc9cf532360
Brandon Lim <limws.brandon@gmail.com>
2021-12-01 19:09:14 +0800

    Add spotlight

---
+++ src/main/java/com/layhill/roadsim/gameengine/graphics/models/Spotlight.java
@@ -0,0 +1,24 @@
+package com.layhill.roadsim.gameengine.graphics.models;
+
+import org.joml.Vector3f;
+
+public class Spotlight extends Light{
+
+    private Vector3f direction;
+    private float radius;
+
+    public Spotlight(Vector3f position, Vector3f direction, Vector3f colour, float radius){
+        super(position, colour);
+        this.direction = direction;
+        this.radius = radius;
+    }
+
+
+    public Vector3f getDirection() {
+        return direction;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+}
