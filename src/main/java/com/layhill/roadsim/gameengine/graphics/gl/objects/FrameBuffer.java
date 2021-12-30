f2e6fe35afd6ab36128c0b01f9110c36181a3c29
Brandon Lim <limws.brandon@gmail.com>
2021-12-31 00:04:45 +0800

    Implement frambuffer builder

--- src/main/java/com/layhill/roadsim/gameengine/graphics/gl/objects/FrameBuffer.java
+++ src/main/java/com/layhill/roadsim/gameengine/graphics/gl/objects/FrameBuffer.java
@@ -1,4 +1,26 @@
 package com.layhill.roadsim.gameengine.graphics.gl.objects;
 
 public class FrameBuffer {
+    private int frameBufferId;
+    private int width;
+    private int height;
+
+    FrameBuffer(int frameBufferId, int width, int height) {
+        this.frameBufferId = frameBufferId;
+        this.width = width;
+        this.height = height;
+    }
+
+
+    public int getFrameBufferId() {
+        return frameBufferId;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
--- src/main/java/com/layhill/roadsim/gameengine/graphics/gl/objects/FrameBufferBuilder.java
+++ src/main/java/com/layhill/roadsim/gameengine/graphics/gl/objects/FrameBufferBuilder.java
@@ -1,12 +1,12 @@
 package com.layhill.roadsim.gameengine.graphics.gl.objects;
 
-import static org.lwjgl.opengl.GL30.glGenFramebuffers;
+import static org.lwjgl.opengl.GL30.*;
 
 public class FrameBufferBuilder {
 
-    private int frameBuffer;
-    private float width;
-    private float height;
+    private int frameBufferId;
+    private int width;
+    private int height;
     private boolean isBuilding;
 
     public FrameBufferBuilder(){
@@ -18,7 +18,32 @@ public class FrameBufferBuilder {
             throw new IllegalStateException("Still building a framebuffer.");
         }
         isBuilding = true;
-        frameBuffer  = glGenFramebuffers();
+        frameBufferId = glGenFramebuffers();
         return this;
+    }
+
+    public FrameBufferBuilder bindFrameBuffer(){
+        checkIsBuildingState();
+        glBindFramebuffer(GL_FRAMEBUFFER, frameBufferId);
+        return this;
+    }
+
+    public FrameBufferBuilder sizeFrameBuffer(int width, int height){
+        checkIsBuildingState();
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    public FrameBuffer build(){
+
+        isBuilding = false;
+        return new FrameBuffer(frameBufferId, width, height );
+    }
+
+    private void checkIsBuildingState(){
+        if (!isBuilding){
+            throw new IllegalStateException("Framebuffer is still under construction");
+        }
     }
 }
